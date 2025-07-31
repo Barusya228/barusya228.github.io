0bf7d230b754d9462e31e5b39a83cfbaeb24fa5d
Barusya228 <barys0804@gmail.com>
2025-07-31 19:26:27 +0500

    Update script.js

--- script.js
+++ script.js
@@ -85,16 +85,6 @@ Telegram.WebApp.onEvent("mainButtonClicked", function(){
 	tg.sendData(item);
 });
 
-
-let usercard = document.getElementById("usercard");
-
-let p = document.createElement("p");
-
-p.innerText = `${tg.initDataUnsafe.user.first_name}
-${tg.initDataUnsafe.user.last_name}`;
-
-usercard.appendChild(p);
-
 document.addEventListener('DOMContentLoaded', () => {
   const order = {}; // { [productId]: count }
 
